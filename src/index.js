e8c1678692eb76bdd95107aa9ef3f6bdd336bd2d
boadu_samuel <samuel@zuberipay.com>
2021-11-07 14:32:11 +0000

    added vue, bubble and common js to rollup config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 export {default as zTable} from './zTable.vue'
 export {default as dropDown} from './dropDown.vue'
-export {default as zSelect} from './zSelect.vue'
+export {default as ZSelect} from './zSelect.vue'
