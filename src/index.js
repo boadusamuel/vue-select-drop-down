b27d160f6e6a1ab061354568393663eff252d91e
boadu_samuel <samuel@zuberipay.com>
2021-11-09 13:03:02 +0000

    removed select sub components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-export {default as ZTable} from './zTable.vue'
-export {default as DropDown} from './dropDown.vue'
-export {default as ZSelect} from './zSelect.vue'
+export {default as ZSelectDropdown} from './zSelect.vue'
